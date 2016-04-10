50e72033758bf420f6cfdebd32d08a7e95e433a3
sephirothwzc <zhanchao.wu@icloud.com>
2016-04-10 14:08:10 +0800

    neusoft demo

---
+++ XMD1/XMD1/MDPage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XMD1
+{
+    /// <summary>
+    /// 主界面
+    /// </summary>
+    public class MDPage: MasterDetailPage
+    {
+        
+    }
+}
